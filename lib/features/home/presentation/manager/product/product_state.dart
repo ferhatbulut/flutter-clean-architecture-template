35de55cf5e7c011bc71b1a9ddba195c8f019f5a2
MokarmDev <mokarmdev@gmail.com>
2026-05-25 00:55:25 +0300

    refactor(home): update product imports in product state

--- lib/features/home/presentation/manager/product/product_state.dart
+++ lib/features/home/presentation/manager/product/product_state.dart
@@ -1,6 +1,6 @@
 import 'package:equatable/equatable.dart';
 
-import '../../../domain/entities/product_entity.dart';
+import '../../../domain/entities/product/product_entity.dart';
 
 abstract class ProductState extends Equatable {
   const ProductState();
