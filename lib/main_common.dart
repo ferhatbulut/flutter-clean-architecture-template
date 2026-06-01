008a04eabc86cbb633dd50c8932a9df567c4009b
MokarmDev <mokarmdev@gmail.com>
2026-06-01 09:47:47 +0300

    feat(main): initialize and set AppBlocObserver in mainCommon entrypoint

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_flavor/flutter_flavor.dart';
 
+import 'core/bloc/app_bloc_observer.dart';
 import 'core/constants/app_strings.dart';
 import 'core/di/injection_container.dart';
 import 'my_app.dart';
@@ -9,7 +12,9 @@ import 'shared/wrappers/localization_wrapper.dart';
 
 Future<void> mainCommon({required String name, required String baseUrl}) async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  if (kDebugMode) {
+    Bloc.observer = AppBlocObserver();
+  }
   await initCore();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -34,7 +39,7 @@ Future<void> mainCommon({required String name, required String baseUrl}) async {
 // to generate env
 // dart run build_runner build
 // to generate feature
-// mason make feature --feature_name home --entity_name Home --output-dir lib
+// mason make feature --feature_name home --entity_name Home 
 // to generate assets
 // dart pub global run flutter_gen:flutter_gen_command
 // or add Pub Cache\bin to PATH then: fluttergen
